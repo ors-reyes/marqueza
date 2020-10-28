bdc8286ec7678e483d50d7cc380c0ac61b7f52f8
ors-reyes <67842856+ors-reyes@users.noreply.github.com>
2020-10-28 00:28:12 -0300

    Add files via upload

--- v3/js/sl.js
+++ v3/js/sl.js
@@ -1,1 +1,12 @@
-
+let actual = 0;
+    function mostrar(n){
+        let img = document.getElementsByClassName("icon")        
+        for(i = 0; i< img.length; i++){
+            if(img[i].className.includes("actual")){
+                img[i].className = img[i].className.replace("actual", "")
+                break;
+            }
+        }
+        actual = n;
+        img[n].className += " actual"        
+    }
