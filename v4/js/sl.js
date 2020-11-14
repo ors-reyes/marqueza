dd1117730560bc02843656cf6526bd20a20f6172
ors-reyes <67842856+ors-reyes@users.noreply.github.com>
2020-11-14 01:34:28 -0300

    Add files via upload

--- v4/js/sl.js
+++ v4/js/sl.js
@@ -1,1 +1,35 @@
+let slPosicion = 0,
+    slVelocidad = 12000,
+    slPlay = setInterval("siguiente()", slVelocidad),
+    slSvg = document.getElementsByClassName("svgIcon"),
+    imgSl = document.getElementsByClassName("imgSl")
+siguiente = () => {
+    slPosicion++;
+    if(slPosicion > slSvg.length-1) slPosicion = 0;
+    mostrar(slPosicion);
+}
+mostrar = (n) => {
+    clearInterval(slPlay);
+    slPlay = null;
 
+    for( let slItem in slSvg){
+        if(slSvg[slItem].className.includes("slActual")){
+            
+            // slSvg[slItem].className = slSvg[slItem].className.replace("slActual", "")
+
+            setTimeout(function(){ 
+                slSvg[slItem].className = slSvg[slItem].className.replace("slActual", "")
+             }, 500);
+
+            imgSl[slItem].className = imgSl[slItem].className.replace("imgActual", "")
+            
+            slPlay = setInterval("siguiente()", slVelocidad)            
+            break
+        }
+    }
+    slPosicion = n;
+    setTimeout(function(){ 
+        slSvg[n].className += " slActual"        
+     }, 500);
+    imgSl[n].className += " imgActual"
+}
